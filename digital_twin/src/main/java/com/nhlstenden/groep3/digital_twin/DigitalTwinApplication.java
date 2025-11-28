8308025a88b18e72a75554fd0076d0b72ff9ae90
Ian Bervoets <ian.bervoets@live.nl>
2025-11-28 15:49:27 +0100

    added generated spring project

---
+++ digital_twin/src/main/java/com/nhlstenden/groep3/digital_twin/DigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.nhlstenden.groep3.digital_twin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalTwinApplication.class, args);
+	}
+
+}
